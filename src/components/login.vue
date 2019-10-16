6f007c16e96a26452ec49b24e816955b7df1cb10
tong-meng <tongchuanmeng@outlook.com>
2019-10-16 16:58:16 +0800

    登录功能完成

---
+++ src/components/login.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="login">
+    <el-form :rules="rules" ref="form" :model="form" status-icon label-width="80px">
+      <img src="../assets/panda.png" alt="">
+      <el-form-item label="用户名" prop="username">
+        <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input placeholder="请输入密码" type="password" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="loginBtn" @click="login">登录</el-button>
+        <el-button @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        // required: true 为必填项
+        // message 提示消息
+        // trigger: blur 失去焦点触发校验   change 修改时就触发
+        // min: 最小长度,  max: 最大长度
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 12, message: '用户名长度必须是3-12位', trigger: ['change', 'blur'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 12, message: '密码长度必须是3-12位', trigger: ['change', 'blur'] }
+        ]
+      }
+    }
+  },
+  methods: {
+    reset () {
+      this.$refs.form.resetFields()
+    },
+    login () {
+    // 获取到 el-form组件, 调用校验方法
+    // 参数1: 是否校验成功  boolean
+    // 参数2: 错误的字段信息
+      this.$refs.form.validate(isValid => {
+        if (!isValid) return
+        // 校验成功
+        console.log('发送登录ajax请求')
+        console.log(this.form)
+        axios.post('http://localhost:8888/api/private/v1/login', this.form).then(
+          res => {
+            // console.log(res)
+            // console.log(res.data)
+            const { meta, data } = res.data
+            if (meta.status === 200) {
+              // 登录成功，跳转页面
+              console.log(meta.msg)
+              localStorage.setItem('token', data.token)
+              this.$message({
+                message: '登录成功',
+                type: 'success',
+                duration: 1000
+              })
+              this.$router.push({ name: 'index' })
+            } else {
+              // 给用户提示，登录失败
+              console.log(meta.msg)
+              this.$message.error('登录失败')
+            }
+          }
+        )
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.login {
+  width: 100%;
+  height: 100%;
+  background-color: #9d9d9d;
+  overflow: hidden;
+  .el-form {
+    position: relative;
+    width: 400px;
+    background-color: #fff;
+    padding: 20px;
+    padding-top: 70px;
+    margin: 0 auto;
+    margin-top: 200px;
+    border-radius: 20px;
+    img {
+      position: absolute;
+      left: 50%;
+      top: -55px;
+      transform: translateX(-50%);
+      width: 90px;
+      height: 90px;
+      border-radius: 50%;
+      border: 1px solid #fff;
+    }
+  }
+  .loginBtn {
+    margin-right: 60px;
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './components/login.vue'
+import Index from './components/index.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/index'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/index',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
+
   ]
 })
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.path === '/login' || token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
+
+export default router
